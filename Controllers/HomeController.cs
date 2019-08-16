d716ad5dbc4f97e8e908483de53f23dd9942f0f4
Joshua <joshua.richardson@myunidays.com>
2019-08-16 11:39:00 +0100

    Learning to use stripe

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LearningStripeCore.Models;
+using Stripe;
+using Newtonsoft.Json;
+
+namespace LearningStripeCore.Controllers
+{
+
+    public class ConfirmPaymentRequest
+    {
+        [JsonProperty("payment_method_id")]
+        public string PaymentMethodId { get; set; }
+
+        [JsonProperty("payment_intent_id")]
+        public string PaymentIntentId { get; set; }
+    }
+
+    // AJAX endpoint when `/ajax/confirm_payment` is called from client
+    [Route("/ajax/confirm_payment")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index([FromBody] ConfirmPaymentRequest request)
+        {
+            var paymentIntentService = new PaymentIntentService();
+            PaymentIntent paymentIntent = null;
+
+            try
+            {
+                if (request.PaymentMethodId != null)
+                {
+                    // Create the PaymentIntent
+                    var createOptions = new PaymentIntentCreateOptions
+                    {
+                        PaymentMethodId = request.PaymentMethodId,
+                        Amount = 1099,
+                        Currency = "gbp",
+                        ConfirmationMethod = "manual",
+                        Confirm = true,
+                    };
+                    paymentIntent = paymentIntentService.Create(createOptions);
+                }
+                if (request.PaymentIntentId != null)
+                {
+                    var confirmOptions = new PaymentIntentConfirmOptions{};
+                    paymentIntent = paymentIntentService.Confirm(
+                        request.PaymentIntentId,
+                        confirmOptions
+                    );
+                }
+            }
+            catch (StripeException e)
+            {
+                return Json(new { error = e.StripeError.Message });
+            }
+            return generatePaymentResponse(paymentIntent);
+          }
+
+          private IActionResult generatePaymentResponse(PaymentIntent intent)
+          {
+            // Note that if your API version is before 2019-02-11, 'requires_action'
+            // appears as 'requires_source_action'.
+            if (intent.Status == "requires_action" &&
+                intent.NextAction.Type == "use_stripe_sdk")
+            {
+                // Tell the client to handle the action
+                return Json(new
+                {
+                    requires_action = true,
+                    payment_intent_client_secret = intent.ClientSecret
+                });
+            }
+            else if (intent.Status == "succeeded")
+            {
+                // The payment didn’t need any additional actions and completed!
+                // Handle post-payment fulfillment
+                return Json(new { success = true });
+            }
+            else
+            {
+                // Invalid status
+                return StatusCode(500, new { error = "Invalid PaymentIntent status" });
+            }
+        }
+    }
+}
+//     }
+// }
+
+        // public IActionResult Charge(PaymentIntent pi)
+        // {
+        //     return View();
+        // }
+
+        // public IActionResult Index()
+        // {
+        //     StripeConfiguration.ApiKey = "sk_test_JSYiBJm8SlRLnoKp7lm4pFOo00Z8SvfVpO";
+        //     var service = new PaymentIntentService();
+            
+        //     var options = new PaymentIntentCreateOptions
+        //     {
+        //         Amount = 111,
+        //         Currency = "gbp",
+        //     };
+
+        //     var intent = service.Create(options);
+        //     // var intent = service.Get("pi_1F7gnVDNI8WLPlOB3H7Xfqjz");
+        //     ViewData["ClientSecret"] = intent.ClientSecret;
+        //     ViewData["Lol"] = intent.Id;
+        //     return View();
+        // }
+
+
+//         public IActionResult IndexPost(string id)
+//         {
+//             StripeConfiguration.ApiKey = "sk_test_JSYiBJm8SlRLnoKp7lm4pFOo00Z8SvfVpO";
+//             var service = new PaymentIntentService();
+//             var options = new PaymentIntentCreateOptions
+//             {
+//                 Amount = 111,
+//                 Currency = "gbp",
+//             };
+            
+//             service.Create(options);
+
+//             // var id = options.RequestId;
+//             var intent = service.Get("pi_1F7gnVDNI8WLPlOB3H7Xfqjz");
+
+            
+//             // System.Console.WriteLine(ViewData["Id"].ToString());
+//             ViewData["ClientSecret"] = intent.ClientSecret;
+//             return View();
+//         }
+
+//         public class PaymentIntentDto
+//         {
+//             [JsonProperty("id")]
+//             public string Id { get; set; }
+//         }
+
+//         public IActionResult Privacy()
+//         {
+//             return View();
+//         }
+
+//         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//         public IActionResult Error()
+//         {
+//             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//         }
+//     }
+// }
---
+++ wwwroot/js/site.js
@@ -0,0 +1,104 @@
+﻿// // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// // for details on configuring this project to bundle and minify static web assets.
+
+// // Write your JavaScript code.
+var stripe = Stripe('pk_test_XfHGe7CQlIfC0oNDVBe7RGgH00XUi5JkVy');
+
+var elements = stripe.elements();
+// var cardElement = elements.create('card');
+
+var cardnumberElement = elements.create('cardNumber');
+var cvcElement = elements.create('cardCvc');
+var expiryElement = elements.create('cardExpiry');
+var zipElement = elements.create('postalCode');
+
+// cardElement.mount('#card-element');
+
+cardnumberElement.mount('#cardNumber-element');
+cvcElement.mount('#cardCvc-element');
+expiryElement.mount('#cardExpiry-element');
+zipElement.mount('#postalCode-element');
+
+var cardholderName = document.getElementById('cardholder-name');
+var cardButton = document.getElementById('card-button');
+var clientSecret = cardButton.dataset.secret;
+
+cardButton.addEventListener('click', function(ev) {
+    ev.preventDefault;
+    cardButton.setAttribute('disabled', 'disabled');
+
+    if(cardholderName.value == "")
+    {
+        cardholderName.classList.add('error');
+        cardButton.removeAttribute('disabled', 'disabled');
+        return;
+    }
+
+    stripe.createPaymentMethod( 'card', cardnumberElement, { 
+        billing_details: {name: cardholderName.value}
+    }).then(function(result){
+        if (result.error)
+        {
+            console.log("Card error");
+            alert("Error");
+        }
+        else
+        {
+            fetch('/ajax/confirm_payment', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                body: JSON.stringify({ PAYMENT_METHOD_ID: result.paymentMethod.id })
+            }).then(function(result) {
+                result.json().then(function(json) {
+                    handleServerResponse(json);
+                })
+            });
+        }
+        cardButton.removeAttribute('disabled', 'disabled');
+    });
+});
+
+function handleServerResponse(response) {
+    if (response.error) {
+        alert('error');
+      // Show error from server on payment form
+    } else if (response.requires_action) {
+      // Use Stripe.js to handle required card action
+      alert('requires action');
+      stripe.handleCardAction(
+        response.payment_intent_client_secret
+      ).then(function(result) {
+        if (result.error) {
+            alert('error');
+          // Show error in payment form
+        } else {
+          // The card action has been handled
+          // The PaymentIntent can be confirmed again on the server
+          fetch('/ajax/confirm_payment', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ payment_intent_id: result.paymentIntent.id })
+          }).then(function(confirmResult) {
+            return confirmResult.json();
+          }).then(handleServerResponse);
+        }
+      });
+    } else {
+      // Show success message
+      alert('success');
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
